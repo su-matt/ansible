396f3a062a50b1c786a6a9eb5934bff9fd12d3c8
su-matt <devprojex@gmail.com>
2024-03-10 20:35:32 -0700

    script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,35 @@
 #! bin/bash
 
-# Install Ansible
-pip install ansible
+# Function to install Homebrew on macOS
+install_homebrew() {
+  test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+  test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+}
 
-ansible-playbook -K bootstrap.yml
+# Check if the OS is Ubuntu
+if [[ "$(uname -s)" == "Linux" ]] && [[ -f /etc/os-release ]] && grep -q "Ubuntu" /etc/os-release; then
+    sudo apt update
+    sudo apt install -y git ansible
+
+    install_homebrew
+
+# Check if the OS is Arch Linux
+elif [[ "$(uname -s)" == "Linux" ]] && command -v pacman &> /dev/null; then
+    sudo pacman -Syu --noconfirm
+    sudo pacman -S --noconfirm git ansible
+
+# Check if the OS is macOS
+elif [[ "$(uname -s)" == "Darwin" ]]; then
+    if ! command -v brew &> /dev/null; then
+        echo "Homebrew is not installed. Installing Homebrew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    brew install git ansible
+
+else
+    echo "Unsupported operating system."
+    exit 1
+fi
+
+ansible-playbook --vault-id stop@prompt --vault-id project@prompt bootstrap.yml
